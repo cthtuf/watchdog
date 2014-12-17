b13cfec564949322f89bd577a8383478ef5f6b06
cthtuf <gdazeysinc@gmail.com>
2014-12-17 09:57:11 +0300

    trying to get favicon

--- watchdog/watchdog/spiders/ollyshap.py
+++ watchdog/watchdog/spiders/ollyshap.py
@@ -10,6 +10,10 @@ class OllyshapSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-    	print response.xpath('//title')
-    	print response.xpath('//meta')
-    	print response.xpath('//link')
+    	#print response.xpath('//title')
+    	#print response.xpath('//meta')
+    	for sel in response.xpath('//link'):
+    		print sel.extract()
+    		#print sel.xpath('/@rel').extract()
+    		#if sel.xpath('/@rel').extract() == 'shortcut icon':
+    		#	print 'busted!'
